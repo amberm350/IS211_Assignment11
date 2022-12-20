91b84da93169f6d3e87b028fba92f9d70953ebae
amberm350 <112363236+amberm350@users.noreply.github.com>
2022-12-20 16:36:11 -0500

    Add files via upload

--- IS211_11/todoapp.py
+++ IS211_11/todoapp.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect, url_for
-import re
+
 
 app = Flask(__name__)
 
@@ -11,45 +11,31 @@ list_todo = [
 
 @app.route('/')
 def home():
+    print(list_todo)
     return redirect(url_for("todolist"))
 
 
 @app.route('/todolist', methods = ['POST', 'GET'])
 def todolist():
-    return render_template('todolisttemplate.html', list_todo=list_todo)
-
-
-@app.route('/submit', methods=['POST', 'GET'])
-def todo():
-    if request.method == 'POST':
-        email = request.form['email']
-        regex = r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
-        if (re.fullmatch(regex, email)):
-            pass
-        else:
-            return redirect(url_for("todolist"))
-
-        difficulty = request.form['difficulty']
+    if request.method =='POST':
+        task = request.values.get('task')
+        email = request.values.get('email')
+        difficulty = request.values.get('difficulty')
         
-        if(difficulty != 'low'):
-            return redirect(url_for("todolist"))
-        if(difficulty != 'medium'):
-            return redirect(url_for("todolist"))
-        if(difficulty != 'high'):
+        
+        if ('@' not in email):
             return redirect(url_for("todolist"))
     
-        else:
-            todo = request.form['todolist']
-            list_todo.append(todo)
-            print(list_todo)
-            return render_template('todolisttemplate.html', list_todo=list_todo)
-
+        
+        list_todo.append({"task": task, "email" : email,"difficulty": difficulty})
+        
+    return render_template('todolisttemplate.html', list_todo=list_todo)
 
 @app.route('/clear')
 def clearlist():
     list_todo.clear()
     return redirect(url_for("todolist"))
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
 
