256681f10b0603abba25beac2eb9fefe140c185f
amberm350 <112363236+amberm350@users.noreply.github.com>
2022-12-20 11:08:31 -0500

    Add files via upload

---
+++ IS211_11/todoapp.py
@@ -0,0 +1,67 @@
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+import re
+
+app = Flask(__name__)
+
+list_todo = [
+    "We need to buy groceries",
+    "We need to mow the lawn",
+    "We need to wash the dishes"
+]
+
+@app.route('/')
+def home():
+    return redirect(url_for("todolist"))
+
+
+@app.route('/todolist', methods = ['POST', 'GET'])
+def todolist():
+    return render_template('todolisttemplate.html', list_todo=list_todo)
+
+
+
+@app.route('/addtolist', methods=['POST', 'GET'])
+def todo():
+    if request.method == 'POST':
+        todo = request.form['todolist']
+        list_todo.append(todo)
+        print(list_todo)
+        return render_template('todolisttemplate.html', list_todo=list_todo)
+
+@app.route('/submit',methods= ['POST','GET'])
+def verifytodo():
+    if request.method == 'POST':
+        email = request.form['email']
+        regex = r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
+        if (re.fullmatch(regex, email)):
+            pass
+        else:
+            return redirect(url_for("todolist"))
+
+        difficulty = request.form['difficulty']
+        
+        if(difficulty != 'low'):
+            return redirect(url_for("todolist"))
+        if(difficulty != 'medium'):
+            return redirect(url_for("todolist"))
+        if(difficulty != 'high'):
+            return redirect(url_for("todolist"))
+    
+        else:
+            task = request.form['task']
+            list_todo.append(task)
+            print(list_todo)
+            return render_template('todolisttemplate.html', list_todo=list_todo)
+    return redirect(url_for("todolist"))
+
+@app.route('/clear')
+def clearlist():
+    list_todo.clear()
+    return redirect(url_for("todolist"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
